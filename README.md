5adce60f4d2dc7d3221e71f464e88b2211f67e09
philogicae <38438271+philogicae@users.noreply.github.com>
2023-09-26 21:42:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Paginated-EnumerableSet
+
+Modified version of EnumerableSet library with pagination.
+
+## Installation
+
+```bash
+npm install paginated-enumerableset
+```
+
+## Use
+
+```solidity
+import "paginated-enumerableset/contracts/PaginatedEnumerableSet.sol";
+```
+
+## License
+
+This project is released under the MIT License.
---
+++ contracts/Example.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./PaginatedEnumerableSet.sol";
+
+contract Example {
+    using PaginatedEnumerableSet for PaginatedEnumerableSet.UintSet;
+    PaginatedEnumerableSet.UintSet private set;
+
+    function add(uint256 x) public returns (bool) {
+        return set.add(x);
+    }
+
+    function remove(uint256 x) public returns (bool) {
+        return set.remove(x);
+    }
+
+    function contains(uint256 x) public view returns (bool) {
+        return set.contains(x);
+    }
+
+    function length() public view returns (uint256) {
+        return set.length();
+    }
+
+    function at(uint256 index) public view returns (uint256) {
+        return set.at(index);
+    }
+
+    function values() public view returns (uint256[] memory result) {
+        result = set.values();
+    }
+
+    function subset(
+        uint256 start,
+        uint256 end
+    ) public view returns (uint256[] memory result) {
+        result = set.subset(start, end);
+    }
+}
